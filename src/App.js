002adfac27374d0bb8f962ac6d08d5b76cef2f4a
Lillevenn <lillevenn@Lillevenns-MacBook-Pro.local>
2021-08-31 12:45:43 -0600

    removed material

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import React from 'react';
 import './App.css';
+import { useEffect, useState } from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { Button, Paper } from '@material-ui/core';
+//import RecipeReviewCard from './components/RecipeReviewCard';
+import { DataStore } from '@aws-amplify/datastore';
+import { Custome } from './models';
+
+
 function App() {
+
+const [custs, setCusts] = useState([])
+
+
+useEffect(() => {
+  const func = async () => {
+    const models = await DataStore.query(Custome)
+    setCusts(models)
+  }
+
+  func()
+
+}, [])
+
+  const createCustomer = async () => {
+   const cust = {
+    name: window.prompt('cust name'),
+    address: window.prompt('address'),
+    phone: window.prompt('phone'),
+    email: window.prompt('email address')
+   }
+
+  const newCust = await DataStore.save(
+    new Custome(cust)
+  )
+
+  console.log(newCust)
+  }
+
   return (
+
+    <div className='App'>
+     <button onClick={createCustomer}>Create Customer</button> 
+     
+     {custs.map(cust => <div key={cust.id}>
+     <h1>{cust.name}</h1>
+     <p>{cust.address}</p>
+    </div>
+    )}
+    </div>
+/*
     <Container maxWidth="sm" className="App">
       <Paper>
         <Typography variant="h4" component="h1" gutterBottom>
@@ -19,6 +65,7 @@ function App() {
         </Button>
       </Paper>
     </Container>
+    */
   );
 }
 export default App;
