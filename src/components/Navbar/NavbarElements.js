83f987d85f1b736774690a54de42b5160cd6dc25
Lillevenn <lillevenn@Lillevenns-MacBook-Pro.local>
2021-09-16 19:20:02 -0600

    change header size

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -11,8 +11,8 @@ export const Nav = styled.nav`
 `;
 
 export const NavLink = styled(Link)`
-  color: #fff;
-  font-size: 1.9rem;
+  color: #f0f;
+  font-size: 1.8rem;
   display: flex;
   align-items: center;
   text-decoration: none;
