be6fd47a24598418061a2cde0f23327c6ba6877d
Jan Birkelund <Janbirkelund@gmail.com>
2021-12-31 19:01:54 -0700

    updating the about

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -24,26 +24,50 @@ const AboutUs = () => {
       <Navbar toggle={toggle} />
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <AboutUsContent>
-        We are located in the south eastern part of Colorado, about 30 miles
-        south of Lamar. Our ranch is about 10,000 acres and have been owned and
-        operated by three generations of Ranchers.
+        Welcome to Bamber Ranch! We are so pleased we are able to share with you
+        a treasure our family has enjoyed for decades. Bamber Ranch is a labor
+        of love from 4 generations of Colorado cattlemen. Robert D. Bamber (Bob)
+        grew up on his father and mothers Vetile and Nellie’s ranch outside of
+        Pritchett CO. Bob loved helping his parents and working with his
+        brothers and sisters. He often took solo trips on horseback and train to
+        transport cattle to sale.
         <br />
         <br />
-        Our eldest Daughter whom you see featured in the gallery was a rodeo
-        Queen.
+        Bob was drafted into World War II and went to serve his country. Before
+        leaving he married Juanita Mae Bickford who grew up on a neighboring
+        homestead. When Bob returned he and Juanita began buying their own land
+        and cattle to put together Bamber Ranch. Bob and Juanita enjoyed many
+        happy years watching their family which included 3 children, 2
+        daughters, Shirley and Tammy and a son Jack, and ranch grow. Upon Bob’s
+        death his only son Jack took over the ranch along with his wife Sandee
+        and 4 children Tonna, Jaclyn, Bobby and Shannon. Jack, a former rodeo
+        cowboy, race horse owner and true American cattleman continues to care
+        for and grow his herd today.
         <br />
         <br />
-        We pride ourselves on delivering the leanest and best beef from our
-        ranch to your table.
+        Jack cares for his herd with great compassion and pride. A ranch of
+        10,000 acres and 500 cattle takes a cowboy with a big heart. Calving
+        season on the Bamber Ranch comes in the spring and as you know
+        springtime in Colorado can often bring snow and cold temperatures. It
+        was not an uncommon occurrence in the Bamber home for the children to
+        awake a spring morning to find a brand new baby calf (or two!) in their
+        kitchen, too young to be able withstand the cold temperatures and
+        blowing winds. Warmed with towels and often the girls’ hair dries the
+        baby calves would be tenderly (and excitedly) nursed back to health. The
+        new calf, often turned pet, would be bottle fed and when healthy enough
+        returned to the pasture. Jack with his soft heart keeping the animal on
+        the ranch for years to come as they often were named and would still
+        respond to the family who cared for them.
         <br />
         <br />
-        Our oldest son competes in roping and made it to the world championship
-        in Las Vegas last year.
+        On the ranch today, there is a dog named Rooster and we have about 10
+        quarter horses who, like our cattle, roams around freely on our Ranch.
+        Bamber Ranch is not simply about beef production, Bamber Ranch is a
+        labor of love. Family owned and operated. Cattle cared for and driven by
+        Jack, his children and now grandchildren. We are so excited to share our
+        Colorado ranch raised beef with you!
         <br />
         <br />
-        On the ranch there is a dog named Rooster (also pictured) and we have
-        about 10 quarter horses who like our cattle roams around freely on our
-        Ranch.
       </AboutUsContent>
 
       {/* <AboutUsContent>
