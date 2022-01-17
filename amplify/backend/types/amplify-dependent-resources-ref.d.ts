1f9d3cbf71b387ca4a4c75f61e35bb1540e55dd4
Bjorn Birkelund <bjornbirkelund@Bjorns-Mac.local>
2022-01-16 19:26:00 -0700

    added my dependencies.. might need to remove

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,15 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "theranch": {
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        },
-        "meatpriceapi": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
-        }
-    },
     "function": {
         "meatpricefunction": {
             "Name": "string",
@@ -28,18 +17,6 @@ export type AmplifyDependentResourcesAttributes = {
             "SortKeyName": "string",
             "SortKeyType": "string",
             "Region": "string"
-        }
-    },
-    "auth": {
-        "theranch897f53f1": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "CreatedSNSRole": "string"
         }
     }
 }
