98a2c8abf8b489a9433615bf1d2d5f2a9a202566
Bjorn Birkelund <bjornbirkelund@Bjorns-Mac.local>
2022-01-18 13:33:11 -0700

    refactored products code to use mui style instead

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -3,24 +3,86 @@ import { useHistory } from "react-router-dom";
 import Navbar from "../Navbar";
 import Sidebar from "../Sidebar";
 import { productData } from "./data";
-//import { DataStore } from "@aws-amplify/datastore";
 import { API } from "aws-amplify";
+import {makeStyles} from "@material-ui/core/styles";
 
-import {
-  ProductsContainer,
-  ProductWrapper,
-  ProductsHeading,
-  ProductTitle,
-  ProductCard,
-  ProductImg,
-  ProductInfo,
-  ProductDesc,
-  ProductPrice,
-  ProductButton,
-} from "./ProductsElements";
+const useStyles = makeStyles({
+  container: {
+    minHeight: "100vh",
+    padding: "5rem calc((100vw - 1300px) / 2)",
+    background: "#383434",
+    color: "#fff",
+  },
+  content: {
+    marginTop: 50,
+    height: "calc(100vh - 80px)",
+    maxHeight: "100%",
+    padding: "0rem calc((100vw - 1300px) / 2)",
+  },
+  heading: {
+    fontSize: "clamp(2rem, 2.5vw, 3rem)",
+    textAlign: "center",
+    marginBottom: "5rem",
+  },
+  wrapper: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    margin: "0 auto",
+  },
+  card: {
+    margin: "0 2rem",
+    lineHeight: 2,
+    width: "300px",
+  },
+  productInfo: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "2rem",
+    textAlign: "center",
+  },
+  title: {
+    fontWeight: 400,
+    fontSize: "1.5rem",
+  },
+  desc: {
+    marginBottom: "1rem",
+  },
+  price: {
+    marginBottom: "1rem",
+    fontSize: "2rem",
+  },
+  img: {
+    filter: "drop-shadow(0.2rem 0.2rem 0.3rem rgba(0, 0, 0, 0.3))",
+    height: "300px",
+    minWidth: "300px",
+    maxWidth: "100%",
+    boxShadow: "8px 8px #fdc500",
+  },
+  button: {
+    filter: "drop-shadow(0.2rem 0.2rem 0.3rem rgba(0, 0, 0, 0.3))",
+    fontSize: "1rem",
+    padding: "1rem 4rem",
+    border: "none",
+    background: "#e31837",
+    color: "#fff",
+    transition: "0.2 ease-out",
+    '&:hover': {
+      background: "#ffc500",
+      transition: "0.2s ease-out",
+      cursor: "pointer",
+      color: "#000",
+    }
+  },
+
+
+});
 
 const Products = ({ heading }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const classes = useStyles();
   const [quarterPrice, setQuarterPrice] = useState(0);
   const [halfPrice, setHalfPrice] = useState(0);
   const [wholePrice, setWholePrice] = useState(0);
@@ -50,32 +112,32 @@ const Products = ({ heading }) => {
   }, []); // only run this function on the first mount
 
   return (
-    <ProductsContainer>
+    <div className={classes.container}>
       <Navbar toggle={toggle} />
       <Sidebar isOpen={isOpen} toggle={toggle} />
-      <ProductsHeading>Choose your favorite</ProductsHeading>
-      <ProductWrapper>
+      <div className={classes.heading}>Choose your favorite</div>
+      <div className={classes.wrapper}>
         {productData && productData.map((product, index) => {
           return (
-            <ProductCard key={index}>
-              <ProductImg src={product.img} alt={product.alt} />
-              <ProductInfo>
-                <ProductTitle>{product.name}</ProductTitle>
-                <ProductDesc>{product.desc}</ProductDesc>
-                <ProductPrice>
+            <div className={classes.card} key={index}>
+              <img className={classes.img} src={product.img} alt={product.alt}/>
+              <div className={classes.productInfo}>
+                <div className={classes.title}>{product.name}</div>
+                <div className={classes.desc}>{product.desc}</div>
+                <div className={classes.price}>
                   {index === 0 && quarterPrice > 0 && `$${quarterPrice}`}
                   {index === 1 && halfPrice > 0 && `$${halfPrice}`}
                   {index === 2 && wholePrice > 0 && `$${wholePrice}`}
-                </ProductPrice>
-                <ProductButton onClick={routeChange}>
+                </div>
+                <div className={classes.button} onClick={routeChange}>
                   {product.button}
-                </ProductButton>
-              </ProductInfo>
-            </ProductCard>
+                </div>
+              </div>
+            </div>
           );
         })}
-      </ProductWrapper>
-    </ProductsContainer>
+      </div>
+    </div>
   );
 };
 
