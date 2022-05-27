7f1c74b2a39e170ae09e8adedcef9e14c044dbac
Jan Birkelund <Janbirkelund@gmail.com>
2022-05-27 15:18:36 -0600

    minor tweaks to faq

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -34,56 +34,87 @@ const Faq = () => {
   const preso =
     "https://docs.google.com/presentation/d/e/2PACX-1vTYa6R1S2SST8ONxr8PaY6qEANXVUaglv5cOURH0WBij6E8a_kvGU6Z8WLH1gvOVdvfk3hUB7IQwQTc/pub?start=true&loop=true&delayms=10000&slide=id.g128367d75c4_0_15";
 
-  const p1 =
-    "Bamber Ranch is a local Colorado ranch located in Lamar. \n" +
-    "Bamber Ranch cattle are Black Angus cows bred with Lim-Flex bulls. \n" +
-    "Black Angus cattle generally taste better and are more tender than other beef. \n" +
-    "This is attributed to the meat’s marbling-a trait that Black Angus is known for. \n" +
-    "The marbling leads to consistent flavor and juiciness. \n" +
-    "Lim-Flex is a registered Limousin/Angus cross combining the best of two breeds. \n" +
-    "This breed of bulls maintain the marbling characteristics of the Angus while maximizing the yield, growth and muscle\n" +
-    "that is unique to the Limousin breed.\n\n" +
-    "Bamber Ranch beef is ranch raised and grass fed with a corn finish to help maximize marbling \n" +
-    "and bring you quality, tender and juicy meat.\n\n\n" +
-    "There are no growth hormones used on Bamber Ranch \n\n\n" +
-    "Our beef is processed in a USDA approved processing plant and transported directly to your door.\n\n";
+  const p1a = [
+    "Bamber Ranch is a local Colorado ranch located in Lamar.",
+    <br />,
+    "Bamber Ranch cattle are Black Angus cows bred with Lim-Flex bulls. ",
+    <br />,
+    "Black Angus cattle generally taste better and are more tender than other beef. ",
+    <br />,
+    "This is attributed to the meat’s marbling-a trait that Black Angus is known for. ",
+    <br />,
+    "The marbling leads to consistent flavor and juiciness. ",
+    <br />,
+    "Lim-Flex is a registered Limousin/Angus cross combining the best of two breeds. ",
+    <br />,
+    "This breed of bulls maintain the marbling characteristics of the Angus while maximizing the yield, growth and muscle",
+    <br />,
+    "that is unique to the Limousin breed.",
+    <br />,
+    "Bamber Ranch beef is ranch raised and grass fed with a corn finish to help maximize marbling ",
+    <br />,
+    "and bring you quality, tender and juicy meat.",
+    <br />,
+    "There are no growth hormones used on Bamber Ranch ",
+    <br />,
+    "Our beef is processed in a USDA approved processing plant and transported directly to your door.",
+    <br />,
+  ];
 
-  const p2 =
-    "How much freezer space do you need?\n" +
-    "        For a 1/4 beef your need about 4 square feet.\n" +
-    "        For a 1/2 beef your need about 8 square feet.\n" +
-    "        For a 1 beef your need about 16 square feet.\n";
+  const p2a = [
+    "How much freezer space do you need?",
+    <br />,
+    "* For a 1/4 beef your need about 4 square feet. ",
+    <br />,
+    "* For a 1/2 beef your need about 8 square feet.",
+    <br />,
+    "* For a 1 beef your need about 16 square feet.",
+    <br />,
+  ];
 
-  const p3 =
-    "Superior product at a great price\n" +
-    "        ~$8/lbs for ground meat and premium cuts\n" +
-    "        Cut out middle man\n" +
-    "        Store bought grass fed is about $10-12 for ground meat and up to $40 for premium cuts\n";
-  const p4 =
-    "Why Bamber Ranch Beef?\n" +
-    "        Superior Product @ Better Price\n" +
-    "        Colorado Ranch Raised = humanely raised\n" +
-    "        No growth hormones!\n" +
-    "        Grass Fed - corn finished for best marbling and taste.\n" +
-    "        VIP service - we deliver to your door!";
+  const p3a = [
+    "Superior product at a great price",
+    <br />,
+    "* ~$8/lbs for ground meat and premium cuts",
+    <br />,
+    "* Cut out the middle man",
+    <br />,
+    "* Store bought grass fed is about $10-12 for ground meat and up to $40 for premium cuts",
+    <br />,
+  ];
+
+  const p4a = [
+    "Why Bamber Ranch Beef?",
+    <br />,
+    "* Superior Product @ Better Price",
+    <br />,
+    "* Colorado Ranch Raised = humanely raised",
+    <br />,
+    "* No growth hormones!",
+    <br />,
+    "* Grass Fed - corn finished for best marbling and taste.",
+    <br />,
+    "* VIP service - we deliver to your door!",
+  ];
+
   return (
     <div className={classes.container}>
       <Navbar toggle={toggle} />
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <div className={classes.content}>
         <Typography variant="body1">
-          {p1}
+          {p1a}
           <br />
           <br />
           <br />
-          {p2}
+          {p2a}
           <br />
           <br />
           <br />
-          {p3}
+          {p3a}
           <br />
           <br />
-          {p4}
+          {p4a}
           <br />
           <br />
         </Typography>
