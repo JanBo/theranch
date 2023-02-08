fe02458c8d0474d1e3c9ff6854a165bee24828fa
@janbo123 <janbirkelund@gmail.com>
2023-02-07 18:29:22 -0700

    changing images

--- src/components/Gallery/Images.js
+++ src/components/Gallery/Images.js
@@ -23,39 +23,11 @@ export const Images = [
   },
   {
     image:
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_0705.jpeg",
+  },
+  {
+    image:
       "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_1766.jpeg",
-  },
-  {
-    image:
-      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_3433.jpeg",
-  },
-  {
-    image:
-      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_2539.jpeg",
-  },
-  {
-    image:
-      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_3439.jpeg",
-  },
-  {
-    image:
-      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_4884.jpeg",
-  },
-  {
-    image:
-      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_0666.jpeg",
-  },
-  {
-    image:
-      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_2889.jpeg",
-  },
-  {
-    image:
-      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_2895.jpeg",
-  },
-  {
-    image:
-      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_0702.jpeg",
   },
   {
     image:
@@ -63,6 +35,42 @@ export const Images = [
   },
   {
     image:
-      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_3701.jpeg",
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_0707.jpeg",
+  },
+  {
+    image:
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_2498.jpeg",
+  },
+  {
+    image:
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_9236.jpeg",
+  },
+  {
+    image:
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_3816.jpeg",
+  },
+  {
+    image:
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_3829.jpeg",
+  },
+  {
+    image:
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_3818.jpeg",
+  },
+  {
+    image:
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_3439.jpeg",
+  },
+  {
+    image:
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_3821.jpeg",
+  },
+  {
+    image:
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_3819.jpeg",
+  },
+  {
+    image:
+      "https://bamberranch2021.s3.us-east-2.amazonaws.com/images/IMG_4884.jpeg",
   },
 ];
